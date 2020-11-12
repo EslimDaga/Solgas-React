35b4b6fee1ff54e5c0a973ab607b2792843c2d97
Eslim Daga <esliim9811@gmail.com>
2020-11-12 17:28:47 -0500

    Fixed errors

--- src/Views/Dashboard/index.jsx
+++ src/Views/Dashboard/index.jsx
@@ -1,10 +1,9 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/role-supports-aria-props */
 import React, { Component } from "react";
-import { BrowserRouter as Link } from "react-router-dom"
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
+//Style
+import "./assets/css/dashboard.css";
 
 class index extends Component {
   render() {
@@ -23,8 +22,97 @@ class index extends Component {
                 </div>
               </div>
               <div className="row layout-top-spacing">
-                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                  Dashboard
+                <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing">
+                  <div className="widget widget-card-four">
+                    <div className="widget-content">
+                      <div className="w-content">
+                        <div className="w-info">
+                          <h6 className="value">210</h6>
+                          <p className="">Eventos</p>
+                        </div>
+                        <div className="">
+                          <div className="w-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
+                              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="progress">
+                        <div className="progress-bar bg-gradient-primary" role="progressbar" style={{width: "57%"}} aria-valuenow="57" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing">
+                  <div className="widget widget-card-four">
+                    <div className="widget-content">
+                      <div className="w-content">
+                        <div className="w-info">
+                          <h6 className="value">150</h6>
+                          <p className="">Checkpoints</p>
+                        </div>
+                        <div className="">
+                          <div className="w-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
+                              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="progress">
+                        <div className="progress-bar bg-gradient-success" role="progressbar" style={{width: "57%"}} aria-valuenow="57" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing">
+                  <div className="widget widget-card-four">
+                    <div className="widget-content">
+                      <div className="w-content">
+                        <div className="w-info">
+                          <h6 className="value">150</h6>
+                          <p className="">Conductores</p>
+                        </div>
+                        <div className="">
+                          <div className="w-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
+                              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="progress">
+                        <div className="progress-bar bg-gradient-warning" role="progressbar" style={{width: "57%"}} aria-valuenow="57" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing">
+                  <div className="widget widget-card-four">
+                    <div className="widget-content">
+                      <div className="w-content">
+                        <div className="w-info">
+                          <h6 className="value">300</h6>
+                          <p className="">Unidades</p>
+                        </div>
+                        <div className="">
+                          <div className="w-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
+                              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="progress">
+                        <div className="progress-bar bg-gradient-danger" role="progressbar" style={{width: "57%"}} aria-valuenow="57" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
