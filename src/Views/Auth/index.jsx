6e0d9c35ba00a5b06c6db5da8805aef83667070f
Eslim Daga <esliim9811@gmail.com>
2020-11-12 12:19:19 -0500

    Add link and fix classes and svg properties

--- src/Views/Auth/index.jsx
+++ src/Views/Auth/index.jsx
@@ -1,64 +1,69 @@
 import React, { Component } from "react";
 import Logo from "./assets/images/logo_solgas.png";
+import { Link } from "react-router-dom";
+//Import Styles
 import "./assets/css/Login.css";
+
 class index extends Component {
   render() {
     return (
-      <div class="form-container">
-        <div class="form-form">
-          <div class="form-form-wrap">
-            <div class="form-container">
-              <div class="form-content">
-                <img src={Logo} width="250" className="logo-login"/>
-                <form class="text-left">
-                  <div class="form">
-                    <div id="username-field" class="field-wrapper input">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+      <div className="form-container">
+        <div className="form-form">
+          <div className="form-form-wrap">
+            <div className="form-container">
+              <div className="form-content">
+                <img src={Logo} width="250" className="logo-login" alt="Logo"/>
+                <form className="text-left">
+                  <div className="form">
+                    <div id="username-field" className="field-wrapper input">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user">
                         <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                         <circle cx="12" cy="7" r="4"></circle>
                       </svg>
-                      <input id="username" name="username" type="text" class="form-control" placeholder="Nombre de Usuario"/>
+                      <input id="username" name="username" type="text" className="form-control" placeholder="Nombre de Usuario"/>
                     </div>
-                    <div id="password-field" class="field-wrapper input mb-2">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
+                    <div id="password-field" className="field-wrapper input mb-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-lock">
                         <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                         <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
                       </svg>
-                      <input id="password" name="password" type="password" class="form-control" placeholder="Contraseña"/>
+                      <input id="password" name="password" type="password" className="form-control" placeholder="Contraseña"/>
                     </div>
-                    <div class="d-sm-flex justify-content-between">
-                      <div class="field-wrapper toggle-pass">
-                        <p class="d-inline-block">Mostrar Contraseña</p>
-                        <label class="switch s-primary">
-                          <input type="checkbox" id="toggle-password" class="d-none"/>
-                          <span class="slider round"></span>
+                    <div className="d-sm-flex justify-content-between">
+                      <div className="field-wrapper toggle-pass">
+                        <p className="d-inline-block">Mostrar Contraseña</p>
+                        <label className="switch s-primary">
+                          <input type="checkbox" id="toggle-password" className="d-none"/>
+                          <span className="slider round"></span>
                         </label>
                       </div>
-                      <div class="field-wrapper">
-                        <button type="submit" class="btn btn-primary" value="">Iniciar Sesión</button>
+                      <div className="field-wrapper">
+                        <Link to="/">
+                          <button type="submit" className="btn btn-primary" value="">Iniciar Sesión</button>
+                        </Link>
                       </div>
                     </div>
-                    <div class="field-wrapper text-center keep-logged-in">
-                      <div class="n-chk new-checkbox checkbox-outline-primary">
-                        <label class="new-control new-checkbox checkbox-outline-primary">
-                          <input type="checkbox" class="new-control-input"/>
-                          <span class="new-control-indicator"></span>Mantenme Conectado
+                    <div className="field-wrapper text-center keep-logged-in">
+                      <div className="n-chk new-checkbox checkbox-outline-primary">
+                        <label className="new-control new-checkbox checkbox-outline-primary">
+                          <input type="checkbox" className="new-control-input"/>
+                          <span className="new-control-indicator"></span>Mantenme Conectado
                         </label>
                       </div>
                     </div>
-                    <div class="field-wrapper">
-                      <a href="auth_pass_recovery.html" class="forgot-pass-link">¿Olvido su contraseña?</a>
+                    <div className="field-wrapper">
+                      <a href="auth_pass_recovery.html" className="forgot-pass-link">¿Olvido su contraseña?</a>
                     </div>
                   </div>
                 </form>
-                <p class="terms-conditions">© 2020 Todos los derechos Reservados. <a href="https://solgas.com.pe">Solgas</a> es un producto de <a href="https://segursat.com">Segursat</a>. <a href="/login">Preferencias de cookies </a>.</p>
+                <p className="terms-conditions">© 2020 Todos los derechos Reservados. <a href="https://solgas.com.pe">Solgas</a> es un producto de <a href="https://segursat.com">Segursat</a>. <a href="/login">Preferencias de cookies </a>.</p>
               </div>
             </div>
           </div>
         </div>
-        <div class="form-image">
-            <div class="l-image">
-            </div>
+        <div className="form-image">
+          <div className="l-image">
+          </div>
         </div>
     </div>
     );
