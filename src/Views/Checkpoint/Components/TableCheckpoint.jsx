dd8fc1e134a1e234ee88966668da0c2cbdd2c67d
Segursat-Peru <edaga@segursat.com>
2020-11-20 15:59:09 -0500

    Finish to checkpoints

---
+++ src/Views/Checkpoint/Components/TableCheckpoint.jsx
@@ -0,0 +1,258 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import cache from "../../../Helpers/cache";
+import { API, sub } from "../../../Constants/global";
+import { MDBDataTable } from 'mdbreact';
+//Import Styles
+import "./../assets/table.css";
+import { Modal,Button } from "react-bootstrap";
+
+const TableCheckpoint = () => {
+
+  //Get Token
+  const token = cache.getItem("user").token;
+
+  const [data,setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [smShow, setSmShow] = useState(false);
+  const [modalEditar, setModalEditar]=useState(false);
+  const [showMap, setShowMap] = useState(false);
+
+  const handleCloseAdd = () => setShow(false);
+  const handleShowAdd = () => setShow(true);
+
+
+  const [consolaSeleccionada, setConsolaSeleccionada] = useState({
+    username : "",
+    created: "",
+    modified: "",
+    name: "",
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setConsolaSeleccionada(prevState => ({
+      ...prevState,
+      [name] : value
+    }));
+  };
+
+  const checkpointDelete = async() => {
+    await Axios.delete(API + sub + `delete-checkpoint/` + consolaSeleccionada.name + "/",{
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${token}`
+      }
+    })
+    .then(response => {
+      setData(data.filter((item) => item.name !== consolaSeleccionada.name));
+      abrirCerrarModalEliminar()
+    })
+  }
+
+  const checkpointAdd = async () => {
+    await Axios.post(API + sub + "create-checkpoint/",consolaSeleccionada,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${token}`
+      }
+    })
+    .then(response => {
+      setData(data.concat(response.data))
+      handleCloseAdd();
+    })
+  }
+
+
+  const checkpointData = async () => {
+    await Axios.get(API + sub + "get-checkpoints/", {
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${token}`
+      }
+    })
+    .then(response => {
+      setData(response.data);
+    })
+  };
+
+  const abrirCerrarModalEditar=()=>{
+    setShowMap(!showMap);
+  }
+
+  const abrirCerrarModalEliminar=()=>{
+    setSmShow(!smShow);
+  }
+
+  const seleccionarConsola=(consola, caso)=>{
+    setConsolaSeleccionada(consola);
+    (caso==='Editar')?abrirCerrarModalEditar():abrirCerrarModalEliminar()
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      await checkpointData();
+    }
+    fetchData();
+  },[]);
+
+  const useCheckpoint = data.map(item => {
+    const container = {};
+    const name = "name";
+    const username = "username";
+    const created = "created";
+    const modified = "modified";
+    const actions = "actions";
+    container[name] = item.name;
+    container[username] = item.username;
+    container[created] = item.created;
+    container[modified] = item.modified;
+    container[actions] = <>
+      <button onClick={()=>seleccionarConsola(item, 'Editar')} className="btn btn-primary">
+        <svg svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-map">
+          <polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon>
+          <line x1="8" y1="2" x2="8" y2="18"></line>
+          <line x1="16" y1="6" x2="16" y2="22"></line>
+        </svg>
+      </button>
+      <button onClick={()=>seleccionarConsola(item, 'Eliminar')} className="btn btn-danger ml-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x-circle">
+          <circle cx="12" cy="12" r="10"></circle>
+          <line x1="15" y1="9" x2="9" y2="15"></line>
+          <line x1="9" y1="9" x2="15" y2="15"></line>
+        </svg>
+      </button>
+    </>
+    return container;
+  })
+
+  const table = {
+    columns: [
+      {
+        label: 'Nombre',
+        field: 'name',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Usuario',
+        field: 'username',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Fecha de Creación',
+        field: 'created',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Fecha de Modificación',
+        field: 'modified',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Acciones',
+        field: 'actions',
+        sort: 'asc',
+        width: 100
+      }
+    ],
+    rows: useCheckpoint
+  };
+
+  return(
+    <div className="row layout-top-spacing">
+      <div id="tableStriped" className="col-lg-12 col-12 layout-spacing">
+        <div className="statbox widget box box-shadow">
+          <div className="widget-header">
+            <div className="row">
+              <div className="col-xl-6 col-md-12 col-sm-12 col-12">
+                <h4>Lista de Conductores</h4>
+              </div>
+              <div className="col-xl-6 col-md-12 col-sm-12 col-12">
+                <div className="d-flex flex-row-reverse bd-highlight">
+                  <button onClick={handleShowAdd}  className="btn btn-primary">
+                    <span>Agregar</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="widget-content widget-content-area">
+            <div className="table-responsive">
+              <MDBDataTable
+                responsive
+                striped
+                bordered
+                small
+                data={table}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal show={show} onHide={handleCloseAdd} dialogClassName = "modal-dialog modal-dialog-centered modal-xl">
+        <Modal.Header closeButton>
+          <Modal.Title style={{color : "#0b2266"}}>Agregar Conductor</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="100%"
+              height="100%"
+              src={`http://checkpoint.segursat.com/api/create-checkpoint/${token}`}>
+            </iframe>
+          </div>
+        </Modal.Body>
+      </Modal>
+      <Modal
+        size="sm"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            Mensaje
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>¿Seguro que desea eliminar el checkpoint <b>{consolaSeleccionada && consolaSeleccionada.name}?</b></Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={() => setSmShow(false)}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={() => checkpointDelete()}>
+            Si
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        show={showMap}
+        onHide={() => setShowMap(false)}
+        dialogClassName = "modal-dialog modal-dialog-centered modal-xl"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Custom Modal Styling {consolaSeleccionada && consolaSeleccionada.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <iframe
+            id="inlineFrameExample"
+            title="Inline Frame Example"
+            width="100%"
+            height="100%"
+            src={`http://checkpoint.segursat.com/api/get-checkpoint/${consolaSeleccionada && consolaSeleccionada.name}/${token}`}>
+          </iframe>
+        </Modal.Body>
+      </Modal>
+    </div>
+  )
+};
+
+export default TableCheckpoint;
--- src/Views/Checkpoint/index.jsx
+++ src/Views/Checkpoint/index.jsx
@@ -1,9 +1,8 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/role-supports-aria-props */
 import React, { Component } from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
+import TableCheckpoint from "./Components/TableCheckpoint";
 
 class index extends Component {
   render() {
@@ -21,11 +20,7 @@ class index extends Component {
                   <h3>Checkpoint</h3>
                 </div>
               </div>
-              <div className="row layout-top-spacing">
-                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                  Checkpoint
-                </div>
-              </div>
+              <TableCheckpoint />
             </div>
             <Footer />
           </div>
