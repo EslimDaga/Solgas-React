aedd8271698fb0758166b35ef981ce409ae7388a
Segursat-Peru <edaga@segursat.com>
2020-11-20 01:05:51 -0500

    Fixed Unit Component

--- src/Views/Unit/index.jsx
+++ src/Views/Unit/index.jsx
@@ -1,38 +1,32 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/role-supports-aria-props */
-import React, { Component } from "react";
+import React from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
+import TableUnit from "./Components/TableUnit";
 
-class index extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div className="main-container" id="container">
-          <div className="overlay"></div>
-          <div className="search-overlay"></div>
-          <Sidebar />
-          <div id="content" className="main-content">
-            <div className="layout-px-spacing">
-              <div className="page-header">
-                <div className="page-title">
-                  <h3>Unit</h3>
-                </div>
-              </div>
-              <div className="row layout-top-spacing">
-                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                  Unit
-                </div>
+
+const Index = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="main-container" id="container">
+        <div className="overlay"></div>
+        <div className="search-overlay"></div>
+        <Sidebar />
+        <div id="content" className="main-content">
+          <div className="layout-px-spacing">
+            <div className="page-header">
+              <div className="page-title">
+                <h3>Unidades</h3>
               </div>
             </div>
-            <Footer />
+            <TableUnit />
           </div>
+          <Footer />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
-export default index
+export default Index
