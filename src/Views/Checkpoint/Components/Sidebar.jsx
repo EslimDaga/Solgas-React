8da6b73d838320511530afaa081e1fcfc8d9bf74
Segursat-Peru <74069210+Segursat-Peru@users.noreply.github.com>
2020-11-23 16:14:03 -0500

    Change route

--- src/Views/Checkpoint/Components/Sidebar.jsx
+++ src/Views/Checkpoint/Components/Sidebar.jsx
@@ -7,7 +7,7 @@ class Sidebar extends Component {
         <nav id="compactSidebar">
           <ul className="menu-categories">
             <li className="menu">
-              <a href="/" data-active="false" className="menu-toggle">
+              <a href="/dashboard" data-active="false" className="menu-toggle">
                 <div className="base-menu">
                   <div className="base-icons">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
