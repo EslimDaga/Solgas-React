20a2fbf1c383d62a43066a32e461a980cce233d6
Segursat-Peru <74069210+Segursat-Peru@users.noreply.github.com>
2020-11-24 00:17:17 -0500

    Web socket ready

---
+++ src/Views/Events/Components/Back.jsx
@@ -0,0 +1,260 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import cache from "../../../Helpers/cache";
+import { API, sub } from "../../../Constants/global";
+import { MDBDataTable } from 'mdbreact';
+//Import Styles
+import "./../assets/table.css";
+import { Modal,Button } from "react-bootstrap";
+
+const UnitDriver = () => {
+
+  //Get Token
+  const token = cache.getItem("user").token;
+
+  const [data,setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [smShow, setSmShow] = useState(false);
+  const [modalEditar, setModalEditar]=useState(false);
+
+  const handleCloseAdd = () => setShow(false);
+  const handleShowAdd = () => setShow(true);
+
+
+  const [consolaSeleccionada, setConsolaSeleccionada] = useState({
+    dni : "",
+    lastname: "",
+    firstname: "",
+    license_number: "",
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setConsolaSeleccionada(prevState => ({
+      ...prevState,
+      [name] : value
+    }));
+  };
+
+  const driverDelete = async() => {
+    await Axios.delete(API + sub + `delete-driver/` + consolaSeleccionada.dni + "/",{
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${token}`
+      }
+    })
+    .then(response => {
+      setData(data.filter((item) => item.dni !== consolaSeleccionada.dni));
+      abrirCerrarModalEliminar()
+    })
+  }
+
+  const driverAdd = async () => {
+    await Axios.post(API + sub + "create-driver/",consolaSeleccionada,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${token}`
+      }
+    })
+    .then(response => {
+      setData(data.concat(response.data))
+      handleCloseAdd();
+    })
+  }
+
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const unitData = async () => {
+    await Axios.get(API + sub + "get-events/", {
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${token}`
+      }
+    })
+    .then(response => {
+      setData(response.data);
+    })
+  };
+
+  const abrirCerrarModalEditar=()=>{
+    setModalEditar(!modalEditar);
+  }
+
+  const abrirCerrarModalEliminar=()=>{
+    setSmShow(!smShow);
+  }
+
+  const seleccionarConsola=(consola, caso)=>{
+    setConsolaSeleccionada(consola);
+    (caso==='Editar')?abrirCerrarModalEditar():abrirCerrarModalEliminar()
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      await unitData();
+    }
+    fetchData();
+  },[unitData]);
+
+  console.log(data);
+
+  const useDrivers = data.map(item => {
+    const container = {};
+    const provider = "provider";
+    const logistic_operator = "logistic_operator";
+    const unitid = "unitid";
+    const type_of_service = "type_of_service";
+    const checkpoint = "checkpoint";
+    const driver_fullname = "driver_fullname";
+    const datetime = "datetime";
+    const actions = "actions";
+    container[provider] = item.provider;
+    container[logistic_operator] = item.logistic_operator;
+    container[unitid] = item.unitid;
+    container[type_of_service] = item.type_of_service;
+    container[checkpoint] = item.checkpoint;
+    container[driver_fullname] = item.driver_fullname;
+    container[datetime] = item.datetime;
+    container[actions] = <button onClick={()=>seleccionarConsola(item, 'Eliminar')} className="btn btn-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye">
+        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+        <circle cx="12" cy="12" r="3"></circle>
+      </svg>
+    </button>
+    return container;
+  })
+
+  const table = {
+    columns: [
+      {
+        label: 'Proveedor',
+        field: 'provider',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Operador Logístico',
+        field: 'logistic_operator',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Placa',
+        field: 'unitid',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Tipo de Servicio',
+        field: 'type_of_service',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Checkpoint',
+        field: 'checkpoint',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Nombre del Conductor',
+        field: 'driver_fullname',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Fecha de Creación',
+        field: 'datetime',
+        sort: 'asc',
+        width: 100
+      }
+      ,
+      {
+        label: 'Ver Detalle',
+        field: 'actions',
+        sort: 'asc',
+        width: 100
+      }
+    ],
+    rows: useDrivers
+  };
+
+  return(
+    <div className="row layout-top-spacing">
+      <div id="tableStriped" className="col-lg-12 col-12 layout-spacing">
+        <div className="statbox widget box box-shadow">
+          <div className="widget-header">
+            <div className="row">
+              <div className="col-xl-6 col-md-12 col-sm-12 col-12">
+                <h4>Lista de Eventos</h4>
+              </div>
+              <div className="col-xl-6 col-md-12 col-sm-12 col-12">
+                <div className="d-flex flex-row-reverse bd-highlight">
+                  <button onClick={handleShowAdd}  className="btn btn-primary">
+                    <span>Agregar</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="widget-content widget-content-area">
+            <div className="table-responsive">
+              <MDBDataTable
+                responsive
+                striped
+                bordered
+                small
+                data={table}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal
+        size="lg"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            Mensaje
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src={consolaSeleccionada && consolaSeleccionada.images} alt="First slide"/>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="..." alt="Second slide"/>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="..." alt="Third slide"/>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={() => setSmShow(false)}>
+            Cancelar
+          </Button>
+          <Button variant="primary">
+            Si
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
+};
+
+export default UnitDriver;
--- src/Views/Events/Components/TableUnit.jsx
+++ src/Views/Events/Components/TableUnit.jsx
@@ -1,259 +1,239 @@
-import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import React from "react";
 import cache from "../../../Helpers/cache";
-import { API, sub } from "../../../Constants/global";
-import { MDBDataTable } from 'mdbreact';
-//Import Styles
-import "./../assets/table.css";
-import { Modal,Button } from "react-bootstrap";
 
-const UnitDriver = () => {
+export default class TableUnit extends React.Component {
 
-  //Get Token
-  const token = cache.getItem("user").token;
+  state = {
+    events : []
+  }
 
-  const [data,setData] = useState([]);
-  const [show, setShow] = useState(false);
-  const [smShow, setSmShow] = useState(false);
-  const [modalEditar, setModalEditar]=useState(false);
+  async componentDidMount() {
+    const res = await Axios.get('http://checkpoint.segursat.com:8080/control/web/api/get-events/',{
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${cache.getItem("user").token}`
+      }
+    });
+    this.setState({events : res.data})
+    console.log(this.state.events);
+  }
 
-  const handleCloseAdd = () => setShow(false);
-  const handleShowAdd = () => setShow(true);
+  constructor(props) {
+    super(props);
 
+    //  this.state.products = [];
+    this.state = {};
+    this.state.filterText = "";
+    this.state.products = [
+      {
+        provider: '',
+        logistic_operator: '',
+        unitid: '',
+        type_of_service: '',
+        checkpoint: '',
+        driver_fullname: '',
+        datetime : ''
+      }
+    ];
+  }
 
-  const [consolaSeleccionada, setConsolaSeleccionada] = useState({
-    dni : "",
-    lastname: "",
-    firstname: "",
-    license_number: "",
-  });
-
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setConsolaSeleccionada(prevState => ({
-      ...prevState,
-      [name] : value
-    }));
+  handleUserInput(filterText) {
+    this.setState({filterText: filterText});
   };
 
-  /* const driverDelete = async() => {
-    await Axios.delete(API + sub + `delete-driver/` + consolaSeleccionada.dni + "/",{
-      headers : {
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${token}`
-      }
-    })
-    .then(response => {
-      setData(data.filter((item) => item.dni !== consolaSeleccionada.dni));
-      abrirCerrarModalEliminar()
-    })
-  }
-
-  const driverAdd = async () => {
-    await Axios.post(API + sub + "create-driver/",consolaSeleccionada,{
-      headers : {
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${token}`
-      }
-    })
-    .then(response => {
-      setData(data.concat(response.data))
-      handleCloseAdd();
-    })
-  }
- */
-
-  const unitData = async () => {
-    await Axios.get(API + sub + "get-events/", {
-      headers : {
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${token}`
-      }
-    })
-    .then(response => {
-      setData(response.data);
-    })
+  handleRowDel(product) {
+    var index = this.state.products.indexOf(product);
+    this.state.products.splice(index, 1);
+    this.setState(this.state.products);
   };
 
-  const abrirCerrarModalEditar=()=>{
-    setModalEditar(!modalEditar);
-  }
-
-  const abrirCerrarModalEliminar=()=>{
-    setSmShow(!smShow);
-  }
-
-  const seleccionarConsola=(consola, caso)=>{
-    setConsolaSeleccionada(consola);
-    (caso==='Editar')?abrirCerrarModalEditar():abrirCerrarModalEliminar()
-  }
-
-  useEffect(() => {
-    async function fetchData() {
-      await unitData();
+  handleAddEvent(evt) {
+    var id = (+ new Date() + Math.floor(Math.random() * 999999)).toString(36);
+    var product = {
+      id: id,
+      provider: "SEGURSAT 2",
+      logistic_operator: 'GINOGAS 2',
+      unitid: 'C6O-731 2',
+      type_of_service: 'GRANEL 2',
+      checkpoint: 'CASITA 2',
+      driver_fullname: 'CAPANI FERNANDEZ, ARTURO 2',
+      datetime : '12/11/2020 08:41 pm 2'
     }
-    fetchData();
-  },[unitData]);
+    this.state.products.push(product);
+    this.setState(this.state.products);
+  }
 
-  console.log(data);
-
-  const useDrivers = data.map(item => {
-    const container = {};
-    const provider = "provider";
-    const logistic_operator = "logistic_operator";
-    const unitid = "unitid";
-    const type_of_service = "type_of_service";
-    const checkpoint = "checkpoint";
-    const driver_fullname = "driver_fullname";
-    const datetime = "datetime";
-    const actions = "actions";
-    container[provider] = item.provider;
-    container[logistic_operator] = item.logistic_operator;
-    container[unitid] = item.unitid;
-    container[type_of_service] = item.type_of_service;
-    container[checkpoint] = item.checkpoint;
-    container[driver_fullname] = item.driver_fullname;
-    container[datetime] = item.datetime;
-    container[actions] = <button onClick={()=>seleccionarConsola(item, 'Eliminar')} className="btn btn-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye">
-        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-        <circle cx="12" cy="12" r="3"></circle>
-      </svg>
-    </button>
-    return container;
-  })
-
-  const table = {
-    columns: [
-      {
-        label: 'Proveedor',
-        field: 'provider',
-        sort: 'asc',
-        width: 150
-      },
-      {
-        label: 'Operador Logístico',
-        field: 'logistic_operator',
-        sort: 'asc',
-        width: 270
-      },
-      {
-        label: 'Placa',
-        field: 'unitid',
-        sort: 'asc',
-        width: 200
-      },
-      {
-        label: 'Tipo de Servicio',
-        field: 'type_of_service',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Checkpoint',
-        field: 'checkpoint',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Nombre del Conductor',
-        field: 'driver_fullname',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Fecha de Creación',
-        field: 'datetime',
-        sort: 'asc',
-        width: 100
+  handleProductTable(evt) {
+    var item = {
+      id: evt.target.id,
+      name: evt.target.name,
+      value: evt.target.value
+    };
+    var products = this.state.products.slice();
+    var newProducts = products.map(function(product) {
+    for (var key in product) {
+      if (key == item.name && product.id == item.id) {
+        product[key] = item.value;
       }
-      ,
-      {
-        label: 'Ver Detalle',
-        field: 'actions',
-        sort: 'asc',
-        width: 100
-      }
-    ],
-    rows: useDrivers
+    }
+    return product;
+  });
+    this.setState({products:newProducts});
+  //  console.log(this.state.products);
   };
-
-  return(
-    <div className="row layout-top-spacing">
-      <div id="tableStriped" className="col-lg-12 col-12 layout-spacing">
-        <div className="statbox widget box box-shadow">
-          <div className="widget-header">
-            <div className="row">
-              <div className="col-xl-6 col-md-12 col-sm-12 col-12">
-                <h4>Lista de Eventos</h4>
-              </div>
-              <div className="col-xl-6 col-md-12 col-sm-12 col-12">
-                <div className="d-flex flex-row-reverse bd-highlight">
-                  <button onClick={handleShowAdd}  className="btn btn-primary">
-                    <span>Agregar</span>
-                  </button>
-                </div>
-              </div>
+  render() {
+    return (
+      <div>
+        <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)}/>
+        <ProductTable onProductTableUpdate={this.handleProductTable.bind(this)} onRowAdd={this.handleAddEvent.bind(this)} onRowDel={this.handleRowDel.bind(this)} products={this.state.products} filterText={this.state.filterText}/>
+      </div>
+    );
+  }
+}
+class SearchBar extends React.Component {
+  handleChange() {
+    this.props.onUserInput(this.refs.filterTextInput.value);
+  }
+  render() {
+    return (
+      <div className="row">
+        <div className="col-lg-6 col-12">
+          <div className="input-group mt-3 mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search">
+                  <circle cx="11" cy="11" r="8"></circle>
+                  <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                </svg>
+              </span>
             </div>
-          </div>
-          <div className="widget-content widget-content-area">
-            <div className="table-responsive">
-              <MDBDataTable
-                responsive
-                striped
-                bordered
-                small
-                data={table}
-              />
-            </div>
+            <input type="text" className="form-control" placeholder="Buscar..." value={this.props.filterText} ref="filterTextInput" onChange={this.handleChange.bind(this)}/>
           </div>
         </div>
       </div>
-      <Modal
-        size="lg"
-        show={smShow}
-        onHide={() => setSmShow(false)}
-        aria-labelledby="example-modal-sizes-title-sm"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-modal-sizes-title-sm">
-            Mensaje
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src={consolaSeleccionada && consolaSeleccionada.images} alt="First slide"/>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="..." alt="Second slide"/>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="..." alt="Third slide"/>
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={() => setSmShow(false)}>
-            Cancelar
-          </Button>
-          <Button variant="primary">
-            Si
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  )
-};
+    );
+  }
+}
 
-export default UnitDriver;
+class ProductTable extends React.Component {
+
+  state = {
+    events : []
+  }
+
+  async componentDidMount() {
+    const res = await Axios.get('http://checkpoint.segursat.com:8080/control/web/api/get-events/',{
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization': `JWT ${cache.getItem("user").token}`
+      }
+    });
+    this.setState({events : res.data})
+    console.log(this.state.events);
+  }
+
+  render() {
+    var onProductTableUpdate = this.props.onProductTableUpdate;
+    var rowDel = this.props.onRowDel;
+    var filterText = this.props.filterText;
+    var product = this.props.products.map(function(product) {
+      if (product.provider.indexOf(filterText) === -1) {
+        return;
+      }
+      return (<ProductRow onProductTableUpdate={onProductTableUpdate} product={product} onDelEvent={rowDel.bind(this)} key={product.id}/>)
+    });
+
+    return (
+      <div>
+      <button type="button" onClick={this.props.onRowAdd} className="btn btn-secondary pull-right mb-3">Agregar</button>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th className="text-center">Proveedor</th>
+              <th className="text-center">Operador Logístico</th>
+              <th className="text-center">Placa</th>
+              <th className="text-center">Tipo de Servicio</th>
+              <th className="text-center">Checkpoint</th>
+              <th className="text-center">Nombre del Conductor</th>
+              <th className="text-center">Fecha de Creación</th>
+              <th className="text-center">Ver Detalle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.events.map(event => <tr className="eachRow">
+        <td className="text-center">
+        { event.provider}
+        </td>
+        <td className="text-center">
+        { event.logistic_operator}
+        </td>
+        <td className="text-center">
+        { event.unitid}
+        </td>
+        <td className="text-center">
+        { event.type_of_service}
+        </td>
+        <td className="text-center">
+        { event.checkpoint}
+        </td>
+        <td className="text-center">
+        { event.driver_fullname}
+        </td>
+        <td className="text-center">
+        { event.datetime}
+        </td>
+        <td className="text-center">
+          <button className="btn btn-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye">
+              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+              <circle cx="12" cy="12" r="3"></circle>
+            </svg>
+          </button>
+        </td>
+      </tr>)}
+            {product}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    return (
+      <tr className="eachRow">
+        <td className="text-center">
+        { this.props.product.provider}
+        </td>
+        <td className="text-center">
+        { this.props.product.logistic_operator}
+        </td>
+        <td className="text-center">
+        { this.props.product.unitid}
+        </td>
+        <td className="text-center">
+        { this.props.product.type_of_service}
+        </td>
+        <td className="text-center">
+        { this.props.product.checkpoint}
+        </td>
+        <td className="text-center">
+        { this.props.product.driver_fullname}
+        </td>
+        <td className="text-center">
+        { this.props.product.datetime}
+        </td>
+        <td className="text-center">
+          <button className="btn btn-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye">
+              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+              <circle cx="12" cy="12" r="3"></circle>
+            </svg>
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
