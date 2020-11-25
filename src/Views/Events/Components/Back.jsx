f7f6dd40bd7f8677df336f337ab0487cb0b2b4c7
Segursat-Peru <edaga@segursat.com>
2020-11-25 09:51:40 -0500

    Setting styles

--- src/Views/Events/Components/Back.jsx
+++ src/Views/Events/Components/Back.jsx
@@ -213,26 +213,6 @@ const UnitDriver = () => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="http://checkpoint.segursat.com:8080//static/pictures/fdd90eda9c94b30d11d98d2cdeb6fb4d.jpg" alt="First slide"/>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="http://checkpoint.segursat.com:8080//static/pictures/eae97854dc8115ec5929911e82670f1c.jpg" alt="Second slide"/>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="http://checkpoint.segursat.com:8080//static/pictures/fdd90eda9c94b30d11d98d2cdeb6fb4d.jpg" alt="Third slide"/>
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Anterior</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Siguiente</span>
-          </a>
           <div class="row">
             <div class="col-xl-6 col-md-12 col-sm-12 col-12">
               <ul class="list-group mt-3">
@@ -271,7 +251,6 @@ const UnitDriver = () => {
             </ul>
             </div>
           </div>
-        </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="danger" onClick={() => setSmShow(false)}>
